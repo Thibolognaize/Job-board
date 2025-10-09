13edbdd6cd8a535e47af2a7596e9b332df8a3987
Thibolognaize <thibault.feat@epitech.eu>
2025-10-09 14:11:24 +0200

    [ADD]_pg-promise module + connection to local db

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://admin:prrrout@localhost:5432/jobboard_db')
 // view engine is ejs
 app.set("view engine", "ejs")
 
