4e8548a88f1c7b2ef083d95ecd19770602a3173d
elifdenizg <edgokturk@protonmail.com>
2025-10-11 18:11:13 +0200

    [DELETE] delete commentaries

--- routes/User.js
+++ routes/User.js
@@ -6,20 +6,3 @@ const controller = require("../controllers/UserController")
 router.get("/", controller.get);
 
 module.exports = router;
-// // Route pour register l'user
-// router.post("/", (req, res) => {
-//     res.send("Formulaire d'enregistrement")
-// });
-
-// // Get a user by his id
-// router
-//     .route("/:id")
-//     .get((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
-//     .put((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
-//     .delete((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
