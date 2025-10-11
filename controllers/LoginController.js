f496bc51eb933ccffed58fd4f01d9069bde58014
elifdenizg <edgokturk@protonmail.com>
2025-10-11 21:37:28 +0200

    [UPDATE] passwords are hashed

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,14 +1,12 @@
 const db = require("../models/db");
 const { errors } = require("pg-promise");
-//const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt"); 
 
 module.exports = {
     login: async (req, res) => {
         try {
             const email_input = req.body.email;
             const password_input = req.body.password;
-
-            console.log(email_input , "--", password_input);
 
             // oneOrNone : soit l'objet soit null
             const user = await db.oneOrNone("SELECT * FROM users WHERE email = $1", [email_input]);
@@ -17,8 +15,9 @@ module.exports = {
                 return res.send("profile not found");
             }
 
-            //acces au donné en pbjet c'est comme ça du coup c'est permis
-            if (user.password === password_input) {
+            const isPasswordValid = await bcrypt.compare(password_input, user.password);
+            
+            if (isPasswordValid) {
                 return res.send("login successful");
             } else {
                 return res.send("password incorrect");
