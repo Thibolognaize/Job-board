fa54eafb81be87b70fd47e5232eb2bb13e6e0946
elifdenizg <edgokturk@protonmail.com>
2025-10-11 18:11:13 +0200

    [DELETE] delete commentaries

--- routes/User.js
+++ routes/User.js
@@ -11,20 +11,3 @@ router.post("/login", (req, res) => {
 });
 
 module.exports = router;
-// // Route pour register l'user
-// router.post("/", (req, res) => {
-//     res.send("Formulaire d'enregistrement")
-// });
-
-// // Get a user by his id
-// router
-//     .route("/:id")
-//     .get((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
-//     .put((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
-//     .delete((req, res) => {
-//         res.send(`Page de profile pour l'id ${req.params.id}`)
-//     })
