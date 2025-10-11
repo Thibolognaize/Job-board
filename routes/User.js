d2497166c61847d7d20d557c575c6c7770e8fedf
elifdenizg <edgokturk@protonmail.com>
2025-10-11 20:09:46 +0200

    [ADD] init register controller

--- routes/User.js
+++ routes/User.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const controller = require("../controllers/UserController")
 const loginController = require("../controllers/LoginController"); //comme notre formulaire recup avec /user/login j'ai ajouté ici
+//const loginController = require("../controllers/RegisterController");
 
 router.get("/", controller.get);
 router.post("/login", loginController.login);
+//router.post("/register", RegisterController.register);
 
 module.exports = router;
