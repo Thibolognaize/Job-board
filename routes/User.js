08002226b465493484a2f3be2e4e072d93b2e6a7
Thibolognaize <thibault.feat@epitech.eu>
2025-10-12 20:47:24 +0200

    [UPDATE]_moved in UserController for login / register

--- routes/User.js
+++ routes/User.js
@@ -2,11 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/UserController")
-const loginController = require("../controllers/LoginController"); //comme notre formulaire recup avec /user/login j'ai ajouté ici
-const registerController = require("../controllers/RegisterController");
 
 router.get("/", controller.get);
-router.post("/login", loginController.login);
-router.post("/register", registerController.register);
+router.post("/login", controller.login);
+router.post("/register", controller.register);
 
 module.exports = router;
