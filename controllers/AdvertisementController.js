4d00e93e211d042fcff198664bbb0a91a1787b49
Thibolognaize <thibault.feat@epitech.eu>
2025-10-13 00:58:03 +0200

    [ADD]_pagination for advertisements

--- controllers/AdvertisementController.js
+++ controllers/AdvertisementController.js
@@ -6,16 +6,46 @@ module.exports = {
     createAdvertisement: (req, res) => {
         res.render("advertisements/create");
     },
+    
     getAll: (req, res) => {
-        const advertisement = db.any("SELECT * FROM advertisements")
+        const page = parseInt(req.query.page) || 1;
+        const limit = 5;
+        const offset = (page - 1) * limit;
+        let totalPages;
+
+        db.one("SELECT COUNT(*) FROM advertisements")
+            .then((count) => {
+                const totalAdvertisements = parseInt(count.count);
+                totalPages = Math.ceil(totalAdvertisements / limit);
+                return db.any(`
+                    SELECT
+                        a.*,
+                        c.name AS company_name,
+                        c.website,
+                        c.contact,
+                        c.address,
+                        c.city
+                    FROM advertisements a
+                    LEFT JOIN companies c ON a.companies_id = c.id
+                    ORDER BY a.id
+                    LIMIT $1 OFFSET $2
+                `, [limit, offset]);
+            })
             .then((advertisements) => {
-                res.render("advertisements/list", { advertisements });
+                res.render("advertisements/list", {
+                    advertisements,
+                    currentPage: page,
+                    totalPages: totalPages
+                });
             })
             .catch((error) => {
-                console.error("Erreur lors de la récupération des annonces :", error);
+                console.error("Erreur :", error);
                 res.status(500).send("Erreur serveur");
             });
     },
+
+
+
     // Récupération du Json d'un seul pour l'affichage des données 
     getAdvertisementJson: (req, res) => {
         const id = req.params.id;
@@ -44,7 +74,7 @@ module.exports = {
     // Submission du formulaire de creation d'advertisement
     post: (req, res) => {
         const { title, description } = req.body;
-        db.none("INSERT INTO advertisements(title, description) VALUES($1, $2)", [title, description])
+        db.none("INSERT INTO advertisements(name, description) VALUES($1, $2)", [title, description])
             .then(() => {
                 // Envoie une réponse avec un message de succès
                 res.render("advertisements/create", {
