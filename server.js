121f1e8bda19736f1c99071205ef16f045c18a02
Thibolognaize <thibault.feat@epitech.eu>
2025-10-13 11:35:53 +0200

    [ADD]_Gestion of session

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const app = express();
 const port = 3000;
-
 const db = require('./models/db');
 
 // middlewares
 app.use(session({
-  secret: "###",
+  secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false,
   cookie: { secure: false } // Mettre a true pour HTTPS
@@ -21,6 +21,13 @@ app.set("view engine", "ejs")
 
 // serve the public folder
 app.use(express.static('public'))
+
+// Middleware pour ajouter les données de la session à chaque vue
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+console.log("Secret Key:", process.env.SECRET_KEY);
 
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(__dirname + '/public/favicon.ico');
