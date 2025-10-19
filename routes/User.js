2ab9f71501772561bcf9118150cc1ada97e075e4
Thibolognaize <thibault.feat@gmail.com>
2025-10-19 22:40:26 +0200

    [UPDATE]_Route and header

--- routes/User.js
+++ routes/User.js
@@ -9,7 +9,7 @@ router.post("/login", controller.login);
 router.get("/register", controller.renderRegister);
 router.post("/register", controller.register); // CREATE
 
-router.get("/profil", controller.renderProfil) //READ
+router.get("/profil/:userId", controller.renderProfil); //READ
 router.post("/profil/:userId", controller.updateProfil); // UPDATE
 router.delete("/profil/:userId", controller.deleteAccount); // DELETE
 
